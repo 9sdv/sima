0ba21c23ec4b5a23f9c2481f077c714ac83886c2
jack <9sdv@protonmail.com>
2018-01-16 10:13:54 -0500

    specify pip2

--- .osx_build.sh
+++ .osx_build.sh
@@ -16,7 +16,7 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "py35" ]]; then brew linkapps python3; fi
 brew install homebrew/science/hdf5
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "py27" ]]; then
-  sudo pip install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py cython pep8 flake8 sphinx numpydoc picos
+  sudo pip2 install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py cython pep8 flake8 sphinx numpydoc picos
 else
   sudo pip3 install numpy scipy matplotlib scikit-image shapely scikit-learn Pillow future toolz nose h5py cython pep8 flake8 sphinx numpydoc picos
 fi
